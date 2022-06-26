c9e20e3c7e41af604e91513d02b51c099714db09
pyslawa <52594962+pyslawa@users.noreply.github.com>
2022-06-26 21:47:10 +0200

    Update framework.py

--- test_cases/framework.py
+++ test_cases/framework.py
@@ -18,8 +18,8 @@ class Test(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_framework(self):
-        pass
+    def test_print_nice_words(self):
+        print("WELL DONE!!!!!!!!!")
 
     # Element of the first task: Try to search the Internet yourself how to get rid of the error:
     # "DeprecationWarning: executable_path has been deprecated, please pass in a Service object"
